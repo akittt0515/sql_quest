7972f8ecbd7dbfe8d98109c131d7fcbbfefb9a88
hiroaki <sample@com>
2021-07-03 07:19:26 +0900

    table作成_std_data

--- sql_quest_app/admin.py
+++ sql_quest_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import std_data
+
+admin.site.register(std_data)
 
 # Register your models here.
--- sql_quest_app/models.py
+++ sql_quest_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.fields import BooleanField, IntegerField
+
+#基本情報データ
+class std_data(models.Model):
+    name=models.CharField(max_length=100)
+    Lv=models.IntegerField(default=1)
+    job=models.CharField(max_length=50)
+    gender=models.BooleanField
+    job_change_count=models.IntegerField(default=0)
+
 
 # Create your models here.
