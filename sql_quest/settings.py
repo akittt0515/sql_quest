d4d9fb9e12b57111dfa03deba4cd8b063ac3f22a
hiroaki <sample@com>
2021-08-24 21:07:17 +0900

    test

--- sql_quest/settings.py
+++ sql_quest/settings.py
@@ -149,3 +149,5 @@ db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+#testtest
